8a144a11e6a9069be104205fca9ba730d837612b
wang24v <wang24v@ncssm.edu>
2023-11-21 10:22:51 -0500

    Added more buttons and resized the home screen.

--- src/main/java/com/example/finalxcmeettracker/HelloApplication.java
+++ src/main/java/com/example/finalxcmeettracker/HelloApplication.java
@@ -2,7 +2,9 @@ package com.example.finalxcmeettracker;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.Rectangle2D;
 import javafx.scene.Scene;
+import javafx.stage.Screen;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -11,21 +13,16 @@ public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello from Everett!");
+        Rectangle2D screenBounds = Screen.getPrimary().getBounds();
+        Scene scene = new Scene(fxmlLoader.load(), screenBounds.getWidth() - 100, screenBounds.getHeight() - 200);
+        stage.setTitle("Cross Country Meet Tracker");
         stage.setScene(scene);
+        stage.setMaximized(true);
         stage.show();
+
     }
 
     public static void main(String[] args) {
         launch();
-    }
-
-    public static class TestScreen extends Application {
-
-        @Override
-        public void start(Stage stage) throws Exception {
-            FXMLLoader fxmlLoader = new FXMLLoader();
-        }
     }
 }
